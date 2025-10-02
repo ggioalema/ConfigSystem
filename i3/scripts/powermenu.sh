6bf93b0b0491ad4e41a6e643c8e9cc54c958f28a
Alessandro Maggio <alex.may2002@gmail.com>
2025-10-02 15:57:08 +0200

    cambiato powermenu.sh, ora reboot funziona, con sfondo per suspend

--- i3/scripts/powermenu.sh
+++ i3/scripts/powermenu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-chosen=$(echo -e "  Lock\n⏾  Suspend\n\uf021  Reboot\n⏻  Shutdown\n✖  Cancel" | rofi -dmenu -i -p "Power menu")
+chosen=$(echo -e "  Lock\n⏾  Suspend\n  Reboot\n⏻  Shutdown\n✖  Cancel" | rofi -dmenu -i -p "Power menu")
 
 case "$chosen" in
     "  Lock") i3lock -e -f -i ~/Pictures/Sfondo;;
-    "⏾  Suspend") systemctl suspend ;;
-   
-    "\uf021  Reboot") systemctl reboot ;;
+    "⏾  Suspend") 
+        i3lock -e -f -i ~/Pictures/Sfondo
+        systemctl suspend
+        ;;
+    "  Reboot") systemctl reboot ;;
     "⏻  Shutdown") systemctl poweroff ;;
     *) exit 0 ;;
 esac
